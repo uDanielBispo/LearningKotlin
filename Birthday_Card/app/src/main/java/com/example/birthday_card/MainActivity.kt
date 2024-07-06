216d9437e5b338dd0ef9db36af5c07156d25688c
Daniel Bispo <danielbispo3015@hotmail.com>
2024-07-06 15:27:43 -0300

    Finalizando birthday card project, resumindo o aprendizado: Aprendi como colocar imagens, como organizar conteudos em uma box, column e row... bem simples alias, aprendi que deve-se ter um arquivo 'strings.xml' para armazenar os textos das labels, isso ajuda em traduções. Dentre outras coisas que aprendi.

--- Birthday_Card/app/src/main/java/com/example/birthday_card/MainActivity.kt
+++ Birthday_Card/app/src/main/java/com/example/birthday_card/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -14,6 +16,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,9 +32,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             Birthday_CardTheme {
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    GreetingMessage(
-                        message = "Happy Birthday friend",
-                        from = "From Daniel",
+                    GreetingImage(
+                        message = stringResource(R.string.happy_birthday_text),
+                        from = stringResource(R.string.signature_text),
                         modifier = Modifier.padding(8.dp)
                     )
                 }
@@ -42,15 +47,17 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun BirthdayCardPreview() {
     Birthday_CardTheme {
-        GreetingMessage(message = "Happy Birthday My Friend!", from = "From Daniel")
+        GreetingImage(
+            message = stringResource(R.string.happy_birthday_text),
+            from = stringResource(R.string.signature_text))
     }
 }
 
 @Composable
 fun GreetingMessage(message: String, from: String, modifier: Modifier = Modifier){
     Column (
-        modifier = Modifier,
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
     ) {
         Text(
             text = message,
@@ -61,8 +68,29 @@ fun GreetingMessage(message: String, from: String, modifier: Modifier = Modifier
         Text(
             text = from,
             fontSize = 36.sp,
-            modifier = Modifier.padding(16.dp).align(alignment = Alignment.End)
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.CenterHorizontally)
         )
     }
+}
+
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(id = R.drawable.androidparty)
+
+    Box(Modifier) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.8f
+        )
+        GreetingMessage(
+            message = message,
+            from = from,
+        )
+    }
+
 }
 
